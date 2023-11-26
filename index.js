6d8cd2171513f6461db3f8d9809372a570db90b3
Hariel Dananjaya <igedeharieldananjaya026@gmail.com>
2023-11-26 23:38:55 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById('htlogo').addEventListener('click', function() {
+    var targetElement = document.getElementById('porto');
+    targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
+
+document.getElementById('cslogo').addEventListener('click', function() {
+    var targetElement = document.getElementById('porto');
+    targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
+
+document.getElementById('jeslogo').addEventListener('click', function() {
+    var targetElement = document.getElementById('porto');
+    targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
+  
